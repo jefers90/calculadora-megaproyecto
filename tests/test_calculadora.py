caa0a0ca106d0c4121df4f61d1933abaca7d3292
jefers90 <166896926+jefers90@users.noreply.github.com>
2024-10-26 16:15:31 -0500

    Update test_calculadora.py

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -5,15 +5,23 @@ class TestCalculadora(unittest.TestCase):
 
     def test_sumar(self):
         self.assertEqual(sumar(1, 2), 3)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
 
     def test_restar(self):
         self.assertEqual(restar(5, 3), 2)
+        self.assertEqual(restar(3, 5), -2)
+        self.assertEqual(restar(-1, -1), 0)
 
     def test_multiplicar(self):
         self.assertEqual(multiplicar(2, 3), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(0, 5), 0)
 
     def test_dividir(self):
         self.assertEqual(dividir(6, 3), 2)
+        self.assertEqual(dividir(5, 2), 2.5)
+        self.assertEqual(dividir(-6, 3), -2)
 
     def test_dividir_por_cero(self):
         with self.assertRaises(ValueError):
@@ -21,4 +29,3 @@ class TestCalculadora(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
